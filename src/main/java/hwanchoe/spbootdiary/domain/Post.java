d04af9d5d2ba84d6374dcc3963d70f1d7b4aa3de
acupoframen <choe.hwan@gmail.com>
2024-07-05 16:09:41 +0900

    Trying to Add springfox and swaggerconfig

--- src/main/java/hwanchoe/spbootdiary/domain/Post.java
+++ src/main/java/hwanchoe/spbootdiary/domain/Post.java
@@ -8,10 +8,10 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Builder
 public class Post extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
