5ebf883e937fb27fe6dacbf2c2cd9c0a43d2882b
acupoframen <choe.hwan@gmail.com>
2024-07-05 16:10:13 +0900

    Trying to Add springfox and swaggerconfig

---
+++ src/test/java/hwanchoe/spbootdiary/dto/PageRequestDTOTest.java
@@ -0,0 +1,12 @@
+package hwanchoe.spbootdiary.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageRequestDTOTest {
+
+    @Test
+    void getKeyword() {
+    }
+}
